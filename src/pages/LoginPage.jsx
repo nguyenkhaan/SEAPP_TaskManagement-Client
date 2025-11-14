a8ff9754fd2a39795a3d74ad1ae7d1cc59a2b045
Andexenn <phanhuynhhung0707@gmail.com>
2025-11-14 12:38:34 +0700

    Feat(LoginPage): basic UI

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -5,15 +5,34 @@ import TwoColumnLayout from '../layouts/TwoColumnLayout'
 export default function LoginPage()
 {
     const leftContent = (
-        <div className='h-full w-full bg-[#FF6767] pl-[50] text-white'>
-            <h1>Welcome back to NoTask</h1>
-            <p>Continue managing your tasks and boosting your productivity.</p>
+        <div className='h-full w-full bg-[var(--color-primary)] ml-[50] text-white'>
+            <div class='introduction' className='font-[Montserrat]'>
+                <h1 className='text-[54px] w-[555px] '>Welcome back to NoTask</h1>
+                <p>Continue managing your tasks and boosting your productivity.</p>
+            </div>
+
+            <div class='statistic' className='grid-cols-2 grid-rows-2'>
+
+            </div>
         </div>
     )
 
     const rightContent = (
-        <div className='h-full w-full bg-white pr-[50]'>
-            <h1 className='text-[#FF6767] font-[Montserrat]'>NoTask</h1>
+        <div className='h-full w-full bg-white pr-[50] '>
+            <div class="top-content" className='font-[Montserrat]'>
+                <h1 className='text-[#FF6767] '>NoTask</h1>
+                <p className='text-[#403D3D] '>Welcome back! Please sign in to continue</p>
+            </div>
+
+            <div class="input">
+                {/* email */}
+                {/* password */}
+
+            </div>
+
+            <div class="login-btn">
+
+            </div>
         </div>
     )
 
