fd90be1dd0e52d3abc2195300c2e1a65022f35b6
khaan <nguyenkhaan2006@gmail.com>
2025-11-24 01:08:06 +0700

    responsive_team

--- src/components/GroupStastic.jsx
+++ src/components/GroupStastic.jsx
@@ -1,26 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 function GroupStastic({
-    stasticNumber = 7, 
-    title = 'My Teams', 
-    Color = '' 
-})
-{
+    stasticNumber = 7,
+    title = 'My Teams',
+    Color = ''
+}) {
     return (
-        <article 
-            className={`relative w-[301px] h-[147px] py-5 px-6 border rounded-[23px] border-[#B3B3B3]`}
-            style={{color: Color}}
-            >
-            <div className='flex items-center justify-between'>
-                <h2 className={`font-medium text-[20px]  font-[Montserrat] text-black`}>{title}</h2>
-                <div 
-                    className={` w-9 h-9 flex items-center justify-center text-sm rounded-full top-12`}
-                    style={{backgroundColor: Color}}
+        <article
+            className={`relative md:w-[301px] w-[280px] md:h-[147px] h-[120px] md:py-5 py-3 md:px-6 px-4 border rounded-[23px] border-[#B3B3B3] max-md:flex max-md:flex-col max-md:items-start max-md:justify-between`}
+            style={{ color: Color }}
+        >
+            <div className='flex items-center justify-between h-[42px] shrink-0'>
+                <h2 className={`font-medium md:text-[18px] text-base font-[Montserrat] text-black`}>{title}</h2>
+                <div
+                    className={` w-9 h-9 md:flex items-center justify-center text-sm rounded-full top-12 hidden`}
+                    style={{ backgroundColor: Color }}
                 >
                     <i class="fa-brands fa-teamspeak text-white"></i>
                 </div>
             </div>
-            <span className='text-[48px] font-medium'>{stasticNumber}</span>
+                <span className='md:text-[48px] flex-1 text-[38px] font-medium'>{stasticNumber}</span>
         </article>
     )
 }
--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -7,62 +7,51 @@ import GroupStastic from '../components/GroupStastic'
 import GroupCard from '../components/GroupCard'
 import Modal from '../components/modal'
 import { makeCode } from '../services/randomCode'
-function Team() { 
-    const [showModal , setShowModal] = useState(false) 
+function Team() {
+    const [showModal, setShowModal] = useState(false)
     return (
         <WorkingLayout>
             <div className='w-full h-full mb-20'>
-                <h2 className='text-black text-4xl font-semibold'>Teams</h2>
-                <h2 className='text-black  font-medium text-[20px] my-1'>Manage and collaborate with your friends</h2>
-                
-                <input 
-                    className='w-[602px] h-[42px] rounded-xl px-5 text-gray-600 font-medium shadow-[0_4px_10px_rgba(0,0,0,0.1)] focus:shadow-[0_6px_14px_rgba(0,0,0,0.15)] outline-none transition-all duration-300' 
+                <h2 className='text-black md:text-4xl text-3xl font-semibold max-md:px-2'>Teams</h2>
+                <h2 className='text-black max-md:px-2 text-sm font-medium md:text-[20px] my-1'>Manage and collaborate with your friends</h2>
+
+                <input
+                    className='md:w-[602px] w-full md:h-[42px] h-9 md:rounded-xl rounded-lg px-5 text-gray-600 font-medium shadow-[0_4px_10px_rgba(0,0,0,0.1)] focus:shadow-[0_6px_14px_rgba(0,0,0,0.15)] outline-none transition-all duration-300'
                     placeholder='Enter your teamm'
                 />
-                
-                <div className='mt-8 border overflow-y-scroll wrapper border-gray-600 rounded-md py-3 px-4 min-h-[700px] w-full'>
+
+                <div className='mt-8 md:border overflow-x-hidden overflow-y-scroll wrapper border-gray-600 rounded-md py-3 px-4 min-h-[700px] w-full'>
                     <div className='w-full flex items-center justify-between gap-3'>
-                        <GroupStastic stasticNumber={7} title='Total Teams' Color='#7f7373'/>
+                        <GroupStastic stasticNumber={7} title='Total Teams' Color='#7f7373' />
                         <GroupStastic stasticNumber={20} title='Total Tasks' Color='#bd7d7e' />
                         <GroupStastic stasticNumber={39} title='Working Tasks' Color='#109628' />
 
                     </div>
-                    <div className='my-4 w-full min-h-40 py-8 500 grid grid-cols-12 gap-x-6 gap-y-5'>
-                        <div className='col-span-4'>
+                    <div className='my-4 w-full min-h-40 py-8 500 grid grid-cols-12 md:gap-x-6 gap-x-3 md:gap-y-5 gap-y-3'>
+                        <div className='md:col-span-4 col-span-6'>
                             <GroupCard />
                         </div>
-                        <div className='col-span-4'>
+                        <div className='md:col-span-4 col-span-6'>
                             <GroupCard />
                         </div>
-                        <div className='col-span-4'>
+                        <div className='md:col-span-4 col-span-12'>
                             <GroupCard />
                         </div>
-                        <div className='col-span-4'>
-                            <GroupCard />
-                        </div>
-                        <div className='col-span-4'>
-                            <GroupCard />
-                        </div>
-                        <div className='col-span-4'>
-                            <GroupCard />
-                        </div>
-                        <div className='col-span-4'>
-                            <GroupCard />
-                        </div>
+
                     </div>
                 </div>
-                <motion.button 
-                    className='absolute bg-(--color-primary) text-xl shadow-lg text-white font-medium px-5 py-4 rounded-xl top-0 right-0 cursor-pointer'
-                    initial={{scale: 1}} 
-                    whileHover={{scale: 1.08}}
-                    transition={{transition: 'all' , ease: 'easeInOut' , duration: 0.3}}
+                <motion.button
+                    className='absolute bg-(--color-primary) text-sm md:text-xl shadow-lg text-white font-medium md:px-5 md:py-4 py-3 px-2 rounded-md md:rounded-xl md:top-0 top-6 md:right-0 right-4 cursor-pointer'
+                    initial={{ scale: 1 }}
+                    whileHover={{ scale: 1.08 }}
+                    transition={{ transition: 'all', ease: 'easeInOut', duration: 0.3 }}
                     onClick={() => setShowModal(true)}
                     type='button'
                 >
                     + Create team
                 </motion.button>
             </div>
-            {showModal && <Modal showModal={setShowModal} code={makeCode(8)} /> }  
+            {showModal && <Modal showModal={setShowModal} code={makeCode(8)} />}
 
         </WorkingLayout>
     )
