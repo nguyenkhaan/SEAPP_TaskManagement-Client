6369a42023e0667f79b5317b786bf3a2aaae44dd
Andexenn <phanhuynhhung0707@gmail.com>
2025-11-27 16:53:10 +0700

    step responsive

--- src/components/HeaderPhase.jsx
+++ src/components/HeaderPhase.jsx
@@ -7,7 +7,7 @@ function HeaderPhase({
 {
     return (
         <div className='w-full flex items-center justify-center'>
-            <div className='rounded-4xl inline-block w-[368px] h-[68px] leading-[68px] text-center text-[36px] shadow-[6px_0_8px_rgba(0,0,0,0.3)] border bg-[#f8f9fe] border-black px-12 text-3xl font-medium text-[#ff6766]'>
+            <div className='rounded-4xl inline-block lg:w-[368px] lg:h-[68px] w-[320px] h-[55px] lg:leading-[68px] leading-[55px] text-center text-2xl shadow-[6px_0_8px_rgba(0,0,0,0.3)] border bg-[#f8f9fe] border-black lg:px-12 px-8 lg:text-3xl font-medium text-[#ff6766]'>
                 {title}
             </div>
         </div>
--- src/components/StepCard.jsx
+++ src/components/StepCard.jsx
@@ -6,16 +6,16 @@ function StepCard({
   content = "Sign up in seconds and set up your personalized workspace. No credit card required for the free trial.",
 }) {
   return (
-    <article className="w-[431px] relative h-150 rounded-2xl overflow-hidden flex flex-col hover:shadow-lg">
+    <article className="xl:w-[431px] lg:w-[400px] md:w-[500px] w-[430px] relative h-150 rounded-2xl overflow-hidden flex flex-col hover:shadow-lg">
       <div className="w-[70px] h-[70px] flex absolute text-2xl font-medium items-center justify-center text-center rounded-full top-[19px] left-[15px] bg-(--color-primary) text-white">
         {index}
       </div>
       <div className="flex-1 bg-cover bg-center bg-no-repeat bg-black"></div>
-      <div className="p-3 bg-white flex-1 flex items-start justify-center gap-2 flex-col">
+      <div className="p-3 bg-white flex-1 flex items-start justify-start pt-15 gap-2 flex-col">
         <h2 className="text-[32px] font-medium w-full text-(--color-text) text-left font-[Montserrat]">
           {title}
         </h2>
-        <p className="text-left w-[383px] font-normal text-2xl text-gray-600 line-clamp-4">
+        <p className="text-left xl:w-[380px] lg:w-[320px] font-normal text-2xl text-gray-600 line-clamp-4">
           {content}
         </p>
       </div>
