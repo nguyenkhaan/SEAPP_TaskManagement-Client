c8f92dfb9cd28d00f4495073b4aff03d90776290
Andexenn <phanhuynhhung0707@gmail.com>
2025-11-27 17:34:29 +0700

    footer responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,30 +5,30 @@ import FooterList from './FooterList'
 import DarkMode from './DarkMode/DarkMode'
 export default function Footer() {
     return (
-        <footer className='w-[1440px] relative px-16 h-120  pt-18'>
-            <div className='grid grid-cols-12 border-b border-[#989898]'>
-                <div className='col-span-6 flex items-start flex-col justify-start'>
+        <footer className='w-full relative px-16 h-120 pt-18 '>
+            <div className='grid grid-cols-12 lg:pb-0 pb-5 border-b border-[#989898]'>
+                <div className='lg:col-span-6 col-span-12 flex items-start flex-col justify-start'>
                     <Logo width={220} height={58} />
-                    <p className='w-[440px] text-xl h-[168px] text-(--color-text-desc)'>The ultimate task management solution for modern teams.</p>
+                    <p className='w-[440px] text-xl lg:h-[168px] h-[100px] text-(--color-text-desc)'>The ultimate task management solution for modern teams.</p>
                 </div>
-                <div className='col-span-2'>
+                <div className='lg:col-span-2 col-span-4'>
                     <FooterList />
                 </div>
-                <div className='col-span-2'>
+                <div className='lg:col-span-2 col-span-4'>
                     <FooterList />
                 </div>
-                <div className='col-span-2 flex flex-col gap-3 items-start'>
-                    <span className='text-black text-2xl mb-1 font-medium'>Change theme</span>
+                <div className='lg:col-span-2 col-span-4 flex flex-col gap-3 items-start'>
+                    <span className='text-black lg:text-2xl text-xl mb-1 font-medium'>Change theme</span>
                     <DarkMode />
                 </div>
             </div>
 
-            <div className='w-full flex relative pt-10 items-center justify-between'>
-                <span className='block text-xl text-black'>© 2025 NoTask. All rights reserved.</span>
+            <div className='w-full flex relative pt-10 items-center justify-between h-[100px]'>
+                <span className='block lg:text-xl md:text-[16px] text-[14px] text-black'>© 2025 NoTask. All rights reserved.</span>
                 <div className='flex items-center justify-between text-(--color-text-desc) gap-6'>
-                    <span className='text-xl'>Privacy Policy</span>
-                    <span className='text-xl'>Terms of Services</span>
-                    <span className='text-xl'>Cookies Policy</span>
+                    <span className='lg:text-xl md:text-[16px] text-[14px]'>Privacy Policy</span>
+                    <span className='lg:text-xl md:text-[16px] text-[14px]'>Terms of Services</span>
+                    <span className='lg:text-xl md:text-[16px] text-[14px]'>Cookies Policy</span>
                 </div>
                 
             </div>
