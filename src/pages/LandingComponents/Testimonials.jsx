28a0c476543f7785172fabca6e7425a9b38a8fc6
Andexenn <phanhuynhhung0707@gmail.com>
2025-11-27 17:01:13 +0700

    testimonial responsive

--- src/pages/LandingComponents/Testimonials.jsx
+++ src/pages/LandingComponents/Testimonials.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import StatisticCard from '../../components/StatisticCard'
 function Testimonials() {
+    
+    const featureItems = Array.from({length: 3}, (_, i) => ({ id: i }));
+
     return (
-        <section className='landing__section bg-white py-20 flex items-center justify-center'>
-            <div className='w-full grid-cols-12 grid justify-center'>
-                <div className='col-span-4 flex items-center justify-center'>
+        <section className='landing__section bg-white py-20 flex items-center justify-center mb-30'>
+            <div className='xl:w-full grid-cols-12 grid gap-y-6 justify-center mx-auto lg:w-[1000px] w-full'>
+                {/* <div className='col-span-4 flex items-center justify-center'>
                     <StatisticCard />
                 </div>
                 <div className='col-span-4 flex items-center justify-center'>
@@ -13,7 +16,13 @@ function Testimonials() {
                 </div>
                 <div className='col-span-4 flex items-center justify-center'>
                     <StatisticCard />
-                </div>
+                </div> */}
+
+                {featureItems.map(item => (
+                    <div key={item.id} className='lg:col-span-4 col-span-12 flex items-center justify-center'>
+                        <StatisticCard/>
+                    </div>
+                ))}
             </div>
         </section>
     )
