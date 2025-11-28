0cc84ce0d09cb8e5ffea56f2b8760f69e87e33ad
khaan <nguyenkhaan2006@gmail.com>
2025-11-28 14:00:23 +0700

    responsive_setting

--- src/pages/Setting.jsx
+++ src/pages/Setting.jsx
@@ -10,24 +10,27 @@ import Account from './SettingComponents/Account'
 function Setting() {
     return (
         <WorkingLayout>
-            <h2 className='font-md max-md:px-3 text-3xl md:text-4xl'>Settings</h2>
-            <span className='font-md max-md:px-3 block my-2 text-lg md:text-xl'>Manage your profile, preferences, and app settings</span>
+            <div className='w-full max-sm:px-2'>
+                <h2 className='font-md max-md:px-3 text-3xl md:text-4xl'>Settings</h2>
+                <span className='font-md max-md:px-3 block my-2 text-lg md:text-xl'>Manage your profile, preferences, and app settings</span>
 
-            <div className='flex flex-col shadow-[15px_10px_25px_rgba(0,0,0,0.3)] items-start justify-start w-full bg-white min-h-200 mb-16 overflow-hidden border border-gray-500 rounded-xl mt-6'>
-                <SectionSetting>
-                    <>
-                        <Avatar width={110} height={110} />
+                <div className='flex flex-col shadow-[15px_10px_25px_rgba(0,0,0,0.3)] items-start justify-start w-full bg-white min-h-200 mb-16 overflow-hidden border border-gray-500 rounded-xl mt-6'>
+                    <SectionSetting>
+                        <>
+                            <Avatar width={110} height={110} />
 
-                    </>
-                </SectionSetting>
-                {/* Update thong tin ca nhan  */}
-                <UpdatePersonalInformation />
-                {/* Cap nhat lai Mat khau  */}
-                <UpdatePassword />
-                {/* Cap nhat lai theme giao dien  */}
-                <Appearance />
-                {/* quan ly tai khoan  */}
-                <Account />
+                        </>
+                    </SectionSetting>
+                    {/* Update thong tin ca nhan  */}
+                    <UpdatePersonalInformation />
+                    {/* Cap nhat lai Mat khau  */}
+                    <UpdatePassword />
+                    {/* Cap nhat lai theme giao dien  */}
+                    <Appearance />
+                    {/* quan ly tai khoan  */}
+                    <Account />
+                </div>
+
             </div>
 
 
