10cdd900197cf37d3d08a4563b7abd3e7f543712
nguyenkhaan <nguyenkhaan2006@gmail.com>
2025-12-09 02:00:42 +0700

    server_wake_up

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import ReactDOM from 'react-dom'
 import { Route, Routes } from 'react-router'
 import NotFound from './components/NotFound.jsx'
 import routes from './router/router.js'
+import { useQuery , useMutation } from '@tanstack/react-query'
+import api from './api/api.js'
 function App() {
   useEffect(() => {
       const mode = localStorage.getItem("theme");
@@ -19,6 +21,15 @@ function App() {
               .setAttribute("data-app-theme", "light");
       }
   }, []);
+  const testingMutation = useMutation({
+    mutationFn: async () => {
+      const responseData = await api.get('/my-testing') 
+      return responseData
+    }
+  })
+  useEffect(() => {
+    testingMutation.mutate() 
+  } , [])
   return (
     <>
       <Routes>
