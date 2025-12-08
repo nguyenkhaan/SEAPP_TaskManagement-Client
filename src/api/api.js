1c5cb5404d5dbbba3dc059120a0584f013af9427
nguyenkhaan <nguyenkhaan2006@gmail.com>
2025-12-09 00:03:00 +0700

    giam_time_out

--- src/api/api.js
+++ src/api/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const BASE_URL = 'https://seapptaskmanagement-server-production.up.railway.app' 
 const api = axios.create({
     baseURL: BASE_URL, 
-    timeout: 30000
+    timeout: 10000
 }) 
 
 export default api 
