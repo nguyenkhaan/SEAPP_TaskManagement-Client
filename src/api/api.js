b130f5fd56959413012144aaf64c3fa77ab61014
nguyenkhaan <nguyenkhaan2006@gmail.com>
2025-12-09 00:30:37 +0700

    tang_time_out

--- src/api/api.js
+++ src/api/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const BASE_URL = 'https://seapptaskmanagement-server-production.up.railway.app' 
 const api = axios.create({
     baseURL: BASE_URL, 
-    timeout: 10000
+    timeout: 15000
 }) 
 
 export default api 
