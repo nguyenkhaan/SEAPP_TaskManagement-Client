df4970ef9070e71b70c9dbface56994ad7011aad
nguyenkhaan <nguyenkhaan2006@gmail.com>
2025-12-11 00:34:01 +0700

    redirect

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -3,9 +3,14 @@ import ReactDOM from 'react-dom'
 import TwoColumnLayout from '../layouts/TwoColumnLayout'
 import LeftContent from './LoginPageComponents/LeftContent'
 import RightContent from './LoginPageComponents/RightContent'
-
+import checkLogin from '../services/checkLogin'
+import { useNavigate } from 'react-router'
 export default function LoginPage()
 {
+    const navigate = useNavigate() 
+    if (checkLogin()) {
+        navigate('/app/dashboard')
+    }
     let leftContent = window.innerWidth >= 1280 ? <LeftContent/> : false;
     let rightContent = <RightContent/>;
     return (
--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,15 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import TwoColumnLayout from '../layouts/TwoColumnLayout';
-import LeftContent from './SignUpPageComponents/LeftContent';
-import RightContent from './SignUpPageComponents/RightContent';
+import React from "react";
+import ReactDOM from "react-dom";
+import TwoColumnLayout from "../layouts/TwoColumnLayout";
+import LeftContent from "./SignUpPageComponents/LeftContent";
+import RightContent from "./SignUpPageComponents/RightContent";
 
 export default function SignUpPage() {
-    let leftContent = <LeftContent />
-    let rightContent = window.innerWidth >= 1280 ? <RightContent/> : false;
+    const navigate = useNavigate();
+    if (checkLogin()) {
+        navigate("/app/dashboard");
+    }
+    let leftContent = <LeftContent />;
+    let rightContent = window.innerWidth >= 1280 ? <RightContent /> : false;
     return (
         <>
             <TwoColumnLayout left={leftContent} right={rightContent} />
         </>
-    )
+    );
 }
